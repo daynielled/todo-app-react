40003ed48ca24055fa2b63f1a07e82ea97369cf9
DD <ddonegal1@gmail.com>
2024-01-24 16:16:57 -0500

    completed the first portion of further study

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = ({ task, removeTodo, id='1' }) => {
-    return (
-        <div>
+const Todo = ({ id = '1', task, completed, removeTodo, updateTodo }) => {
+    const [isEditing, setEditing] = useState(false);
+    const [editedTask, setEditedTask] = useState(task);
+
+    const handleEditClick = () => {
+        setEditing(true);
+    };
+
+    const handleDelete = () => {
+        removeTodo(id);
+    }
+
+    const handleEditSubmit = (e) => {
+        e.preventDefault();
+        updateTodo(id, editedTask);
+        setEditing(false);
+    };
+
+    const handleMarkAsCompleted = () => {
+        updateTodo(id, "", !completed)
+    };
+
+
+const handleChange = (e) => {
+    setEditedTask(e.target.value);
+}
+
+return (
+    <div>
+        <span style={{ textDecoration: completed ? "line-through" : "none" }}>
             {task}
-            <button onClick={() => removeTodo(id)}>X</button>
-        </div>
-    );
+        </span>
+        <li> {task} </li>
+        <button onClick={handleDelete}>Delete</button>
+        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleMarkAsCompleted}>Mark As Completed</button>
+
+        {isEditing && (
+            <form onSubmit={handleEditSubmit}>
+                <input
+                    type="text"
+                    value={editedTask}
+                    onChange={handleChange}
+                />
+                <button>Update!</button>
+            </form>
+        )}
+    </div>
+  );
 };
 
 export default Todo;
